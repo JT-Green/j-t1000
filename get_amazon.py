2345c1c043da4f86f3e8c247a70df4dc19846564
JT-Green <31543546+JT-Green@users.noreply.github.com>
2017-10-18 17:03:35 -0400

    Revert "Revert "Trying to make put myself out of a job""
    This reverts commit ad242551413cb52c1a6643fd0dba706be08a2bef.

---
+++ get_amazon.py
@@ -0,0 +1,119 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome(r'C:\Program Files\Chromedriver\chromedriver.exe')
+
+login_url = r'https://vendorcentral.amazon.com'
+reports_url = r'https://vendorcentral.amazon.com/st/vendor/members/analytics/basic/productDetail?ref_=vc_ven-badashboard_subNav'
+
+user = 'jt@workman.com'
+pw = 'Workpassword'
+
+driver.get(login_url)
+
+user_field = driver.find_element_by_name('username')
+user_field.send_keys(user)
+
+pw_field = driver.find_element_by_name('password')
+pw_field.send_keys(pw)
+
+login_btn = driver.find_element_by_id('login-button-container')
+login_btn.click()
+
+driver.get(reports_url)
+time.sleep(3)
+
+period_btn = driver.find_element_by_xpath('//*[@id="vxa-ab-reporting-period"]/span/span')
+period_btn.click()
+time.sleep(3)
+
+last_week = driver.find_element_by_xpath('//*[@id="vxa-ab-reporting-period_1"]')
+last_week.click()
+time.sleep(3)
+
+filter_btn = driver.find_element_by_xpath('//*[@id="a-autoid-1-announce"]/span')
+filter_btn.click()
+time.sleep(3)
+
+product_filter = driver.find_element_by_xpath('//*[@id="vxa-ab-product-details"]/div[4]/div[5]/div/div[2]/ul/li[2]')
+product_filter.click()
+time.sleep(3)
+
+books_btn = driver.find_element_by_xpath('//*[@id="lazy-filter-group-product-group"]/div[3]/div[1]/span/div/label/input')
+books_btn.click()
+time.sleep(3)
+
+try:
+    pets_btn = driver.find_element_by_xpath('//*[@id="lazy-filter-group-product-group"]/div[3]/div[3]/span/div/label/input')
+    pets_btn.click()
+    time.sleep(3)
+except:
+    pass
+
+export_btn = driver.find_element_by_xpath('//*[@id="a-autoid-27-announce"]')
+export_btn.click()
+time.sleep(3)
+
+csv_btn = driver.find_element_by_xpath('//*[@id="vxa-ab-export-selector_1"]')
+# Exports Amazon_TW
+csv_btn.click()
+
+
+period_btn.click()
+time.sleep(3)
+
+ytd = driver.find_element_by_xpath('//*[@id="vxa-ab-reporting-period_7"]')
+ytd.click()
+time.sleep(3)
+
+export_btn.click()
+time.sleep(2)
+# Exports Amazon_YTD
+csv_btn.click()
+
+product_filter.click()
+time.sleep(3)
+
+digital_btn = driver.find_element_by_xpath('//*[@id="lazy-filter-group-product-group"]/div[3]/div[2]/span/div/label/input')
+digital_btn.click()
+
+time.sleep(3)
+books_btn.click()
+time.sleep(3)
+
+try:
+    pets_btn.click()
+    time.sleep(3)
+except:
+    pass
+
+export_btn.click()
+time.sleep(3)
+# Exports Kindle_YTD
+csv_btn.click()
+
+period_btn.click()
+time.sleep(3)
+
+last_week.click()
+time.sleep(3)
+
+export_btn.click()
+time.sleep(3)
+# Exports Kindle_TW
+csv_btn.click()
+
+"""
+For after you've downloaded the files
+
+import glob
+import os
+
+files = glob.glob("C:\\Users\\jt\\Downloads\\ProductDetails*.csv")
+files.sort(key=lambda x: os.path.getmtime(x))
+print(files)
+print(files[-1])
+files.pop(-1)
+print(files)
+
+"""
---
+++ get_firebrand.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Chrome(r'C:\Program Files\Chromedriver\chromedriver.exe')
+
+url = "http://tme.firebrandtech.com/WPC/Secure/Login.aspx"
+username = r"jtgreen"
+password = r"Fire044x!"
+
+url2 = "http://tme.firebrandtech.com/WPC/Dialogs/ReportsPopup.aspx?ListKey=12742073&SearchType=6&ReportDataCode=10&ReportDataSubCode=2&ReportDataSub2Code=3&RunReport=true"
+
+if __name__ == "__main__":
+    driver.get(url)
+    uname = driver.find_element_by_name("TemplateWithSingleContentControl$CC$Login1$txtUserName")
+    uname.send_keys(username)
+
+    passw = driver.find_element_by_name("TemplateWithSingleContentControl$CC$Login1$txtPassword")
+    passw.send_keys(password)
+
+    login_btn = driver.find_element_by_name("TemplateWithSingleContentControl$CC$Login1$cmdLogin")
+    login_btn.click()
+
+    driver.close()
+    driver.switch_to.window(driver.window_handles[-1])
+    driver.get(url2)
+
+    export_btn = driver.find_element_by_id("CrystalReportViewer_toptoolbar_export")
+    export_btn.click()
+
+    combobox = driver.find_element_by_class_name('iconnocheck')
+    combobox.click()
+
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.TAB * 4 + Keys.ENTER)
+    actions.perform()
+
+    export_btn2 = driver.find_element_by_class_name('wizbutton')
+    export_btn2.click()
+
